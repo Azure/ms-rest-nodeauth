279c4dc9045d459ab037fe7d53cef442236dd464
Kamil Pajdzik <kamip@microsoft.com>
2018-08-27 15:05:23 -0700

    Setup nock

--- test/credentials/msiTokenCredentialsTests.ts
+++ test/credentials/msiTokenCredentialsTests.ts
@@ -1,10 +1,31 @@
 import { expect } from "chai";
 import "mocha";
+import nock from "nock";
 
-describe("Hello function", () => {
+describe("MSI authentication", () => {
 
-  it("should return hello world", () => {
-    expect("abc").to.equal("Hello world!");
+  function setupNockResponse(port: number, requestBodyToMatch: any, response: any, error: any) {
+    if (!port) {
+      port = 50342;
+    }
+
+    const basePath = `http://localhost:${port}`;
+    const interceptor = nock(basePath).post("/oauth2/token",
+      function (body: any) {
+        return JSON.stringify(body) === JSON.stringify(requestBodyToMatch);
+      });
+
+    if (!error) {
+      interceptor.reply(200, response);
+    } else {
+      interceptor.replyWithError(error);
+    }
+  }
+
+  it("should get token from the virtual machine with MSI service running at default port", () => {
+    setupNockResponse;
   });
+
+
 
 });
