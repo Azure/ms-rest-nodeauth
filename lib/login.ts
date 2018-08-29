a70cd78049fa890e79dadfd49f0abdfd031a6dc2
Kamil Pajdzik <kamip@microsoft.com>
2018-08-28 20:57:35 -0700

    Update MSIOptions and MSIAppServiceOptions

--- lib/login.ts
+++ lib/login.ts
@@ -113,24 +113,48 @@ export interface LoginWithAuthFileOptions {
 }
 
 /**
- * @interface MSIOptions - Describes optional parameters for MSI authentication.
+ * @interface MSIOptions Defines the optional parameters for authentication with MSI.
  */
 export interface MSIOptions {
   /**
-   * @property {number} port - Port on which the MSI service is running on the host VM. Default port is 50342
-   */
-  port?: number;
-  /**
-   * @property {string} resource - The resource uri or token audience for which the token is needed.
+   * @prop {string} [resource] -  The resource uri or token audience for which the token is needed.
    * For e.g. it can be:
-   * - resourcemanagement endpoint "https://management.azure.com"(default)
+   * - resourcemanagement endpoint "https://management.azure.com"(default) 
    * - management endpoint "https://management.core.windows.net/"
    */
   resource?: string;
+}
+
+/**
+ * @interface MSIAppServiceOptions Defines the optional parameters for authentication with MSI for AppService.
+ */
+export interface MSIAppServiceOptions extends MSIOptions {
   /**
-   * @property {string} aadEndpoint - The add endpoint for authentication. default - "https://login.microsoftonline.com"
+   * @property {string} [msiEndpoint] - The local URL from which your app can request tokens.
+   * Either provide this parameter or set the environment varaible `MSI_ENDPOINT`.
+   * For example: `export MSI_ENDPOINT="http://127.0.0.1:41741/MSI/token/"`
    */
-  aadEndpoint?: string;
+  msiEndpoint?: string;
+  /**
+   * @property {string} [msiSecret] - The secret used in communication between your code and the local MSI agent.
+   * Either provide this parameter or set the environment varaible `MSI_SECRET`.
+   * For example: `export MSI_SECRET="69418689F1E342DD946CB82994CDA3CB"`
+   */
+  msiSecret?: string;
+  /**
+   * @property {string} [msiApiVersion] - The api-version of the local MSI agent. Default value is "2017-09-01".
+   */
+  msiApiVersion?: string;
+}
+
+/**
+ * @interface MSIVmOptions Defines the optional parameters for authentication with MSI for Virtual Machine.
+ */
+export interface MSIVmOptions extends MSIOptions {
+  /**
+   * @prop {number} [port] - port on which the MSI service is running on the host VM. Default port is 50342
+   */
+  port?: number;
 }
 
 /**
