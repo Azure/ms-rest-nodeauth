0cc5dc14d5f246d73a4ff2bd1d8205fcd55f890c
Kamil Pajdzik <kamip@microsoft.com>
2018-08-30 14:09:46 -0700

    Update exports

--- lib/msRestNodeAuth.ts
+++ lib/msRestNodeAuth.ts
@@ -1,16 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-export { TokenCredentialsBase, TokenResponse } from "./credentials/tokenCredentialsBase";
+export { TokenCredentialsBase } from "./credentials/tokenCredentialsBase";
 export { ApplicationTokenCredentials } from "./credentials/applicationTokenCredentials";
 export { DeviceTokenCredentials } from "./credentials/deviceTokenCredentials";
 export { UserTokenCredentials } from "./credentials/userTokenCredentials";
-export { MSITokenCredentials, MSITokenResponse } from "./credentials/msiTokenCredentials";
+export { MSIOptions, MSITokenCredentials, MSITokenResponse } from "./credentials/msiTokenCredentials";
+export { MSIAppServiceOptions, MSIAppServiceTokenCredentials } from "./credentials/msiAppServiceTokenCredentials";
+export { MSIVmOptions, MSIVmTokenCredentials } from "./credentials/msiVmTokenCredentials";
 export { AuthConstants, TokenAudience } from "./util/authConstants";
 export { LinkedSubscription, LinkedUser, UserType } from "./subscriptionManagement/subscriptionUtils";
 export {
   AuthResponse, LoginWithAuthFileOptions, InteractiveLoginOptions,
-  MSIOptions, AzureTokenCredentialsOptions, LoginWithUsernamePasswordOptions,
+  AzureTokenCredentialsOptions, LoginWithUsernamePasswordOptions,
   interactive as interactiveLogin,
   withInteractiveWithAuthResponse as interactiveLoginWithAuthResponse,
   withUsernamePassword as loginWithUsernamePassword,
