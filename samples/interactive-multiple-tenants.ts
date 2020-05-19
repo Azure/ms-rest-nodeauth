e2a37172f41bd6513b4aa82ba1c0b4ae880ab952
Daniel Rodriguez <Daniel.Rodriguez@microsoft.com>
2020-05-19 23:23:23 +0000

    fixing the build

--- samples/interactive-multiple-tenants.ts
+++ samples/interactive-multiple-tenants.ts
@@ -1,5 +1,5 @@
 import * as msRestNodeAuth from "../lib/msRestNodeAuth";
-import { SubscriptionClient } from '@azure/arm-subscriptions';
+import { SubscriptionClient } from "@azure/arm-subscriptions";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -7,14 +7,18 @@ dotenv.config();
 // then from the root of the cloned repo run, ts-node ./samples/interactive.ts
 async function main(): Promise<void> {
   try {
-
     // Personal accounts will find no subscriptions on the initial authentication request.
     // This is because the credentials are generated without specifying a tenant.
     const initialAuthentication = await msRestNodeAuth.interactiveLoginWithAuthResponse();
-    console.log("Subscriptions retrieved by default", initialAuthentication.subscriptions);
+    console.log(
+      "Subscriptions retrieved by default",
+      initialAuthentication.subscriptions
+    );
 
     // To get the tenants for your account, you can use the buildTenantList method:
-    const tenants = await msRestNodeAuth.buildTenantList(initialAuthentication.credentials);
+    const tenants = await msRestNodeAuth.buildTenantList(
+      initialAuthentication.credentials
+    );
 
     // If you have already authenticated and you want to retrieve the subscriptions of a specific tenant,
     // you can set the credential's domain to the tenant you want to use,
@@ -25,10 +29,15 @@ async function main(): Promise<void> {
     console.log(`Subscriptions of tenant ${tenants[0]}`, subscriptions);
 
     // You can also authenticate for a known tenant directly:
-    const tenantAuthentication = await msRestNodeAuth.interactiveLoginWithAuthResponse({ domain: tenants[0] });
-    console.log(`Subscriptions of tenant ${tenants[0]}`, tenantAuthentication.subscriptions);
+    const tenantAuthentication = await msRestNodeAuth.interactiveLoginWithAuthResponse(
+      { domain: tenants[0] }
+    );
+    console.log(
+      `Subscriptions of tenant ${tenants[0]}`,
+      tenantAuthentication.subscriptions
+    );
 
-    // If you have already authenticated and you wish 
+    // If you have already authenticated and you wish
   } catch (err) {
     console.log(err);
   }
